a94b659cb15639880b925d49ee5bf9581732905a
Administrator <balanatesh3@gmail.com>
2024-10-05 12:24:27 +0530

    added update product page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'cart', component: CartComponent },
     { path: 'add-product', component: AddProductComponent },
     { path: 'admin-products', component: AdminProductsComponent },
+    { path: 'update-product/:id', component: UpdateProductComponent }, 
 ];
